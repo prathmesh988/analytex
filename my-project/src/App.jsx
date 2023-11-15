fe301257ba6367ab1352d4340c2738e28382e385
Orcus <kusum.aayushi@gmail.comcls>
2023-11-15 14:04:45 +0530

    added 404 page

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,27 +1,37 @@
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import Registration from "./pages/Registration";
 import Stats from "./pages/Stats";
+import NotFoundPage from "./pages/NotFoundPage";
 
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
 // Create a router with your routes
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Registration />,
-  },
-  {
-    path: "/stats",
-    element: <Stats />,
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Registration /> },
+      { path: "/stats", element: <Stats /> },
+      { path: "*", element: <NotFoundPage /> },
+    ],
   },
 ]);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ my-project/src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../layout/Navbar";
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <>
+        <h1 className="text-black text-3xl font-bold pb-4">404 Not Found</h1>
+      </>
+    </div>
+  );
+}
